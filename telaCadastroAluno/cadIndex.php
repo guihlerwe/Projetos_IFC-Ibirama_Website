77a24478d11979fe6306b5bd7c6306e70f524c54
edson-moser <guiihlerwe@icloud.com>
2025-05-27 11:14:39 -0300

    PHP e design updates

---
+++ telaCadastroAluno/cadIndex.php
@@ -0,0 +1,7 @@
+<?php
+    $nome = $_POST["nome"];
+    $sobrenome = $_POST["sobrenome"];
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
+    $curso = $_POST["curso"];    
+?>
---
+++ telaCadastroAluno/cadastrar.php
@@ -0,0 +1,14 @@
+<?php
+    $nome = $_POST["nome"];
+    $sobrenome = $_POST["sobrenome"];
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
+    $curso = $_POST["curso"];
+
+
+
+    echo("Seja bem-vindo $nome $sobrenome");
+
+
+    
+?>
