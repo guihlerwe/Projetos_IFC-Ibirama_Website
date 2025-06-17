03216f981ae88bf8158afd43865ebded96497dae
Gabriella Sandner <gabriellasandner@gmail.com>
2025-06-17 11:49:45 -0300

    ajustes na tela de cad projeto e no footer

---
+++ telaCadastroProjeto/cadProjeto.js
@@ -0,0 +1,8 @@
+var projetos = [];
+
+var nome = document.getElementById("txtNumero").value;
+var anoIncio = document.getElementById("anoInicio").value;
+var categoria = document.getElementsById("categoria").value;
+var coordenador = document.getElementById("coordenador").value;
+var bolsista = document.getElementById("bolsista").value;
+var txtLinkInscricao = document.getElementsByClassName("txtLinkInscricao =").value;
