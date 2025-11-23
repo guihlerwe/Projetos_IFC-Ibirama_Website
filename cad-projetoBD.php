600ff81e403e9e80724c5c4ac793f879071511b9
Guilherme Raimundo <130239914+guihlerwe@users.noreply.github.com>
2025-11-23 17:16:15 -0300

    Update database password

--- cad-projetoBD.php
+++ cad-projetoBD.php
@@ -16,7 +16,7 @@ ini_set('display_errors', 1);
 // banco de dados
 $host = 'localhost';
 $usuario = 'root';
-$senha = 'Gui@15600';
+$senha = 'YOUR_PASSWORD';
 $banco = 'website';
 
 // conexão com o banco
