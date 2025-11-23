cb234b35e1717958c3aebb499f343209fddd894f
Guilherme Raimundo <130239914+guihlerwe@users.noreply.github.com>
2025-11-23 17:20:30 -0300

    Update database password

--- contaBD.php
+++ contaBD.php
@@ -30,7 +30,7 @@ if (!$idPessoa) {
 // Conexão com o banco
 $host = 'localhost';
 $usuario = 'root';
-$senha = 'Gui@15600';
+$senha = 'YOUR_PASSWORD';
 $banco = 'website';
 
 $conn = new mysqli($host, $usuario, $senha, $banco);
